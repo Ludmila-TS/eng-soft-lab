9b444691823c00e619c0da1e03d3443864a90278
Ludmila Tobias <147954239+Ludmila-TS@users.noreply.github.com>
2025-07-06 19:47:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//mensagem ao clicar no botão
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Seja bem-vindo a minha página!";
+    mensagem.style.display = "block";
+}
